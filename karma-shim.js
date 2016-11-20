17c841f6990ab064e88098ad0ca219272730fb78
Curtis Chin <csicartist@gmail.com>
2016-11-20 17:18:23 -0500

    .

--- karma-shim.js
+++ karma-shim.js
@@ -1,20 +1,28 @@
-'use strict';
-
 Error.stackTraceLimit = Infinity;
-require('es6-shim');
-require('angular2/bundles/angular2-polyfills.js');
-require('angular2/testing');
+
+require('core-js/client/shim');
+require('reflect-metadata');
+
+require('ts-helpers');
+
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
 
 /*
  Ok, this is kinda crazy. We can use the the context method on
  require that webpack created in order to tell webpack
  what files we actually want to require or import.
- Below, context will be an function/object with file names as keys.
+ Below, context will be a function/object with file names as keys.
  using that regex we are saying look in client/app and find
- any file that ends with spec.js and get its path. By passing in true
+ any file that ends with '.spec.ts' and get its path. By passing in true
  we say do this recursively
  */
-var appContext = require.context('./src', true, /root\.spec\.ts/);
+var appContext = require.context('./src', true, /\.spec\.ts/);
 
 // get all the files, for each file, call the context function
 // that will require the file and load it up here. Context will
@@ -24,7 +32,7 @@ appContext.keys().forEach(appContext);
 // Select BrowserDomAdapter.
 // see https://github.com/AngularClass/angular2-webpack-starter/issues/124
 // Somewhere in the test setup
-var testing = require('angular2/testing');
-var browser = require('angular2/platform/testing/browser');
+var testing = require('@angular/core/testing');
+var browser = require('@angular/platform-browser-dynamic/testing');
 
-testing.setBaseTestProviders(browser.TEST_BROWSER_PLATFORM_PROVIDERS, browser.TEST_BROWSER_APPLICATION_PROVIDERS);
+testing.TestBed.initTestEnvironment(browser.BrowserDynamicTestingModule, browser.platformBrowserDynamicTesting());
